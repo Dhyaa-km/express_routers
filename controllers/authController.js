8d5892fb1dfc5afd398b4df329d745ac8dc362a9
dhyaa <mohkerrouche9@gmail.com>
2026-07-07 16:11:46 +0100

    add: adding user roles

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,11 +3,11 @@ const usersDB = {
     setUsers: function (data) { this.users = data }
 }
 const bcrypt = require('bcrypt');
+
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const fsPromises = require('fs').promises;
 const path = require('path');
-
 
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
@@ -17,35 +17,33 @@ const handleLogin = async (req, res) => {
     // evaluate password 
     const match = await bcrypt.compare(pwd, foundUser.password);
     if (match) {
-
+        const roles = Object.values(foundUser.roles);
         // create JWTs
         const accessToken = jwt.sign(
-            { username: foundUser.username },
+            {
+                "UserInfo": {
+                    "username": foundUser.username,
+                    "roles": roles
+                }
+            },
             process.env.ACCESS_TOKEN_SECRET,
             { expiresIn: '30s' }
         );
         const refreshToken = jwt.sign(
-            { username: foundUser.username },
+            { "username": foundUser.username },
             process.env.REFRESH_TOKEN_SECRET,
             { expiresIn: '1d' }
         );
-        
-        foundUser.refreshToken = refreshToken;
-        console.log(foundUser.refreshToken);
-        const result = await fsPromises.writeFile(
-            path.join(__dirname, '..', 'model', 'users.json'),
-            JSON.stringify(usersDB.users)
-        );
-        // saving refreshToken with current user
+        // Saving refreshToken with current user
         const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username);
-        const currentUser = { ...foundUser, "refreshToken": refreshToken };
-        usersDB.setUsers([...otherUsers, currentUser])
+        const currentUser = { ...foundUser, refreshToken };
+        usersDB.setUsers([...otherUsers, currentUser]);
         await fsPromises.writeFile(
             path.join(__dirname, '..', 'model', 'users.json'),
             JSON.stringify(usersDB.users)
-        )
-        res.coockie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
-        res.json({ accessToken});
+        );
+        res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });
+        res.json({ accessToken });
     } else {
         res.sendStatus(401);
     }
--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -19,8 +19,15 @@ const handleRefreshToken = (req, res) => {
         process.env.REFRESH_TOKEN_SECRET,
         (err, decoded) => {
             if (err || foundUser.username !== decoded.username) return res.sendStatus(403);
+            const roles = Object.values(foundUser.roles);
             const accessToken = jwt.sign(
-                { "username": decoded.username },
+                {
+                    "UserInfo":
+                    {
+                        "roles": roles,
+                        "username": decoded.username
+                    }
+                },
                 process.env.ACCESS_TOKEN_SECRET,
                 { expiresIn: '30s' }
             );
